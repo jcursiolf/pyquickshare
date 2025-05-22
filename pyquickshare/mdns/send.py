9944bbb95a9d35a6f775b0ea41589f059edecde2
Jair Cursiol Filho <jair.filho@eldorado.org.br>
2025-05-22 11:28:54 -0300

    [win_rt] Updated logic, but still not working

--- pyquickshare/mdns/send.py
+++ pyquickshare/mdns/send.py
@@ -11,10 +11,11 @@ os_name = platform.system()
 if os_name == "Linux":
     from bless.backends.bluezdbus.dbus.advertisement import BlueZLEAdvertisement, Type
     from bless.backends.bluezdbus.server import BlessServerBlueZDBus
+    from dbus_next.signature import Variant
 elif os_name == "Windows":
     # How to make advertisement?
-    from bless.backends.winrt.server import BlessServerWinRT
-from dbus_next.signature import Variant
+    #from bless.backends.winrt.server import BlessServerWinRT
+    from bless import GATTCharacteristicProperties, GATTAttributePermissions, BlessServer
 from zeroconf import IPVersion, ServiceStateChange, Zeroconf
 from zeroconf.asyncio import (
     AsyncServiceBrowser,
@@ -93,31 +94,85 @@ class AsyncRunner:
 async def trigger_devices() -> None:
     if os_name == "Windows":
         # I actually have less than zero clue what I'm doing here`
-        """ FOR THIS COMMIT I JUST COPIED THE LINUX CODE THAT DOES NOT WORK ON LINUX """
+        bluetooth.debug("It is windows!")
+        server = BlessServer(name="pyquickshare_windows")
+        
+        #THERE IS NO SETUP_TASK FOR THE WINRT SERVER
+        #await server.setup_task  # pyright: ignore[reportUnknownMemberType]
+        
+        bluetooth.debug("Connected to whatever WinRT uses")
 
-        server = BlessServerBlueZDBus(name="pyquickshare")
-        await server.setup_task  # pyright: ignore[reportUnknownMemberType]
-        bluetooth.debug("Connected to BlueZ D-Bus")  # Hello :3
+        # THERE IS NO server.app THE WINRT SERVER
+        # await server.app.set_name(server.adapter, server.name)
+        
+        # THERE IS NO BlueZLEAdvertisement EQUIVALENT FOR THE  WINRT SERVER
+        # Type.BROADCAST is defined in bless\backends\bluezdbus\dbus\advertisement.py and has no counterpart in WinRT
+        # There is no server.app in the WinRT server
+        # advertisement = BlueZLEAdvertisement(Type.BROADCAST, 2, server.app)
 
-        await server.app.set_name(server.adapter, server.name)
-        advertisement = BlueZLEAdvertisement(Type.BROADCAST, 2, server.app)
+        # HOW DO I PASS THE SERVICE_UUID to WinRT???
+        # advertisement.ServiceUUIDs = [SERVICE_UUID]S
 
-        advertisement.ServiceUUIDs = [SERVICE_UUID]
-        advertisement.ServiceData = {
-            SERVICE_UUID: Variant("ay", SERVICE_DATA + random.randbytes(9)),  # noqa: S311 - random is fine here
-        }
+        # from https://github.com/Martichou/rquickshare/blob/master/core_lib/src/hdl/ble.rs
+        SERVICE_UUID_SHARING = "0000fe2c-0000-1000-8000-00805f9b34fb"
+        
+        await server.add_new_service(uuid=SERVICE_UUID_SHARING)
+        bluetooth.debug(f"Added new service with UUID {SERVICE_UUID_SHARING}")
+        
+        
+        # HOW DO I PASS THE SERVICE_DATA to WinRT???
+        # Variant is defined on dbus_next\signature.py and clearly on Windows.
+        # advertisement.ServiceData = {
+        #     SERVICE_UUID: Variant("ay", SERVICE_DATA + random.randbytes(9)),  # noqa: S311 - random is fine here
+        # }
 
-        server.app.advertisements = [advertisement]
+        # Add a Characteristic to the service
+        # I have no idea what to put in the char uuid
+        service_char_uuid = "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
+        
+        # I think just broadcast would be enough, but who knows?
+        service_properties = (
+            GATTCharacteristicProperties.read |
+            GATTCharacteristicProperties.write |
+            GATTCharacteristicProperties.indicate
+            )
 
-        server.bus.export(advertisement.path, advertisement)
-
-        iface = server.adapter.get_interface("org.bluez.LEAdvertisingManager1")
-
-        await iface.call_register_advertisement(  # pyright: ignore[reportUnknownMemberType, reportAttributeAccessIssue]
-            advertisement.path,
-            {},
+        # NRF Connect says the Linux version has no flags....
+        service_properties = 0
+        
+        # Got this from https://github.com/rohitsangwan01/ble_peripheral_windows/blob/main/python/ble__handler.py
+        service_permissions = (
+            GATTAttributePermissions.readable |
+            GATTAttributePermissions.writeable
         )
+        
+        # What should the service value be?
+        service_data_value = SERVICE_DATA + random.randbytes(9)
+        bluetooth.debug("Service value?")
+        
+        await server.add_new_characteristic(
+            service_uuid=SERVICE_UUID_SHARING,
+            char_uuid=service_char_uuid,
+            properties=service_properties,
+            value=None,
+            permissions=service_permissions
+            )
+        bluetooth.debug(f"Characteristics added! {server.get_characteristic(service_char_uuid)}")
 
+        
+        # server.app.advertisements = [advertisement]
+
+        # server.bus.export(advertisement.path, advertisement)
+
+        # There is no server.adapter in WinRT backend.
+        # iface = server.adapter.get_interface("org.bluez.LEAdvertisingManager1")
+
+        # await iface.call_register_advertisement(  # pyright: ignore[reportUnknownMemberType, reportAttributeAccessIssue]
+            # advertisement.path,
+            # {},
+        # )
+
+        await server.start()
         bluetooth.debug("Advertising Quick Share service")
 
         # Wait forever, BlueZ keeps advertising while the D-Bus connection is open
@@ -125,7 +180,7 @@ async def trigger_devices() -> None:
     elif os_name == "Linux":
         # I actually have zero clue what I'm doing here
 
-        server = BlessServerBlueZDBus(name="pyquickshare")
+        server = BlessServerBlueZDBus(name="pyquickshare_linux")
         await server.setup_task  # pyright: ignore[reportUnknownMemberType]
         bluetooth.debug("Connected to BlueZ D-Bus")  # Hello :3
 
